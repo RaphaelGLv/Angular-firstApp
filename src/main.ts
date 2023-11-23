037af2a845c83a6911a057dc8533089fba6bbf5e
Raphael L <raphaleiva@gmail.com>
2023-11-23 20:04:52 -0300

    Lessons 7-14

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@
 *  Protractor is used in this example for compatibility with Angular documentation tools.
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import routConfig from './app/routes';
+
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routConfig)
+    ]
+  }
+).catch(err => console.error(err));
